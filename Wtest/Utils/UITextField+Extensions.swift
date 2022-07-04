b51b795a6d9056e89d24d4ed5123efcd85db1253
Yuri <yurihp01@hotmail.com>
2022-07-04 21:35:52 +0100

    add done button

--- Wtest/Utils/UITextField+Extensions.swift
+++ Wtest/Utils/UITextField+Extensions.swift
@@ -5,4 +5,37 @@
 //  Created by Yuri on 04/07/2022.
 //
 
-import Foundation
+import UIKit
+
+extension UISearchBar {
+
+    @IBInspectable var doneAccessory: Bool{
+        get{
+            return self.doneAccessory
+        }
+        set (hasDone) {
+            if hasDone{
+                addDoneButtonOnKeyboard()
+            }
+        }
+    }
+
+    func addDoneButtonOnKeyboard() {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+
+        self.inputAccessoryView = doneToolbar
+    }
+
+    @objc func doneButtonAction() {
+        self.resignFirstResponder()
+    }
+
+}
